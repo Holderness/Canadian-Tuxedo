7093d0beabcd616f68ee2eaf99833bdd4f2cd861
alishak82 <alisha82@gmail.com>
2014-11-18 18:28:36 -0500

    populates clothing_items_controller.rb

--- Juxtapose/app/controllers/clothing_items_controller.rb
+++ Juxtapose/app/controllers/clothing_items_controller.rb
@@ -1,5 +1,37 @@
 class ClothingItemsController < ApplicationController
   def index
-    @clothing_items = ClothingItem.all
+    @clothing_items = current_user.clothing_items
+  end
+
+  def new
+    @clothing_item = ClothingItem.new
+  end
+
+  def create
+    @clothing_item = ClothingItem.create(clothing_item_params)
+  end
+
+  def edit
+    @clothing_item = ClothingItem.find(params[:id])
+  end
+
+  def update
+    clothing_item = ClothingItem.find(params[:id])
+    clothing_item.update(clothing_item_params)
+  end
+
+  def show
+    @clothing_item = ClothingItem.find(params[:id])
+  end
+
+  def destroy
+    clothing_item = ClothingItem.find(params[:id])
+    clothing_item.destroy
+  end
+
+  private
+
+  def clothing_item_params
+    params.require(:clothing_item).permit()
   end
 end
