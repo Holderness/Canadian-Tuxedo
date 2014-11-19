d76f113d752c5afd1ee9cfe4fc2068094a73a721
deniseb89 <denise_b89@hotmail.com>
2014-11-19 14:54:44 -0500

    please work

--- Juxtapose/db/schema.rb
+++ Juxtapose/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141119002448) do
+ActiveRecord::Schema.define(version: 20141119172705) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,11 @@ ActiveRecord::Schema.define(version: 20141119002448) do
   add_index "clothing_tag_assignments", ["clothing_item_id"], name: "index_clothing_tag_assignments_on_clothing_item_id", using: :btree
   add_index "clothing_tag_assignments", ["tag_id"], name: "index_clothing_tag_assignments_on_tag_id", using: :btree
 
+  create_table "comments", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "look_tag_assignments", force: true do |t|
     t.integer  "tag_id"
     t.integer  "look_id"
@@ -62,6 +67,11 @@ ActiveRecord::Schema.define(version: 20141119002448) do
   create_table "users", force: true do |t|
     t.string   "username"
     t.string   "password_digest"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "votes", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
