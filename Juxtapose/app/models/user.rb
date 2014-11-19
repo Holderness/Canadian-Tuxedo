bde31c776d2cd499bc8867e9b9db7af737eb788e
alishak82 <alisha82@gmail.com>
2014-11-19 18:27:48 -0500

    meh

--- Juxtapose/app/models/user.rb
+++ Juxtapose/app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
 
+  has_attached_file :clothing_item, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :clothing_item, :content_type => /\Aimage\/.*\Z/
+
   has_secure_password
   validates_presence_of :password, :on => :create
 
@@ -7,4 +10,5 @@ class User < ActiveRecord::Base
   has_many :looks
   has_many :comments
   has_many :votes
+
 end
