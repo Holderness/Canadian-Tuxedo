3fa31836c535fd42456670b2969b3e77be7625cf
CCLiff <CLCliff30@gmail.com>
2014-11-20 15:31:11 -0500

    Refactors add tags for clothing items. Adds delete for clothing_tag_assignments when deleting a clothing item

--- Juxtapose/app/controllers/clothing_items_controller.rb
+++ Juxtapose/app/controllers/clothing_items_controller.rb
@@ -6,21 +6,26 @@ class ClothingItemsController < ApplicationController
 
   def new
     @clothing_item = ClothingItem.new
+    @tags = @clothing_item.tags
   end
 
   def create
     @clothing_item = ClothingItem.create(clothing_item_params)
-    binding.pry
+    ClothingTagAssignment.add_tags(params, @clothing_item)
     redirect_to clothing_item_path(@clothing_item)
+
   end
 
   def edit
     @clothing_item = ClothingItem.find(params[:id])
+    @tags = @clothing_item.tags
   end
 
   def update
     clothing_item = ClothingItem.find(params[:id])
     clothing_item.update(clothing_item_params)
+    ClothingTagAssignment.add_tags(params, @clothing_item)
+
     redirect_to clothing_item_path(clothing_item)
   end
 
@@ -31,13 +36,15 @@ class ClothingItemsController < ApplicationController
   def destroy
     clothing_item = ClothingItem.find(params[:id])
     clothing_item.destroy
-    redirect_to clothing_items_path
+    ClothingTagAssignment.delete_tags(clothing_item)
   end
 
   private
 
   def clothing_item_params
-    params.require(:clothing_item).permit(:image, :caption, :user_id, :tags)
+    params.require(:clothing_item).permit(:image, :caption, :user_id)
+
+    redirect_to clothing_items_path
   end
 
 end
--- Juxtapose/app/models/clothing_tag_assignment.rb
+++ Juxtapose/app/models/clothing_tag_assignment.rb
@@ -1,4 +1,32 @@
 class ClothingTagAssignment < ActiveRecord::Base
   belongs_to :tag
   belongs_to :clothing_item
+
+  def self.add_tags(params, clothing_item)
+    tags_array = params[:tags].split(",")
+    tags_array.each do |text|
+      text.chomp
+      while text[0] == " " do
+        text[0] = ""
+      end
+      if Tag.find_by(text: text)
+        tag = Tag.find_by(text: text)
+      else
+        tag = Tag.create(text: text)
+      end
+      ClothingTagAssignment.create(tag_id: tag.id, clothing_item_id: clothing_item.id) unless ClothingTagAssignment.find_by(tag_id: tag.id, clothing_item_id: clothing_item.id)
+    end
+  end
+
+  def self.delete_tags(clothing_item)
+    tag_assignments = clothing_item.clothing_tag_assignments
+    tag_assignments_ids = []
+    tag_assignments.each do |tag_assignment|
+      tag_assignments_ids << tag_assignment.id
+    end
+    ClothingTagAssignment.destroy(tag_assignments_ids)
+
+    redirect_to clothing_items_path
+  end
+
 end
