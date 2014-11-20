83f1a265b6c9d50181ed79669836f03b4e761c49
alishak82 <alisha82@gmail.com>
2014-11-19 19:32:09 -0500

    adds clothing item images

--- Juxtapose/app/models/user.rb
+++ Juxtapose/app/models/user.rb
@@ -1,8 +1,5 @@
 class User < ActiveRecord::Base
 
-  has_attached_file :clothing_item, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :clothing_item, :content_type => /\Aimage\/.*\Z/
-
   has_secure_password
   validates_presence_of :password, :on => :create
 
