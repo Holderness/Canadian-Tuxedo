c7566bef5eef16697da3b72a0680f21bfb9e0f12
CCLiff <CLCliff30@gmail.com>
2014-11-21 10:44:07 -0500

    gallery front end creation

--- Juxtapose/config/routes.rb
+++ Juxtapose/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :clothing_items
   resources :looks
+  get '/gallery' => 'looks#gallery', as: 'gallery'
   resources :users
   get '/login' => 'users#login', as: 'login'
   post '/login' => 'sessions#login'
