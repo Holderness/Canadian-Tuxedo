9625bae1d1049a36c6cc05be504b79854927fb16
Holderness <andersonholderness@gmail.com>
2014-11-21 12:11:38 -0500

    e

--- Juxtapose/app/controllers/votes_controller.rb
+++ Juxtapose/app/controllers/votes_controller.rb
@@ -6,14 +6,21 @@ class VotesController < ApplicationController
     user_id = current_user.id
     user_vote = Vote.find_by(look_id: look_id, user_id: user_id)
     if user_vote
+      if user_vote.vote == vote.to_i 
+        vote = 0
+      end
     	user_vote.update(vote: vote)
     else
       Vote.create(vote: vote, look_id: look_id, user_id: user_id)
     end
 
+
+
     respond_to do |format|
       format.json {render :nothing => true}
     end
+
+
 
   end
 
