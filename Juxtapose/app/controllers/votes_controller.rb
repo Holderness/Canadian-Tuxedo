0d77b8b6ad9a1ca10999c1ab635fba4ae4c29011
Holderness <andersonholderness@gmail.com>
2014-11-21 11:02:06 -0500

    creates backend voting system

--- Juxtapose/app/controllers/votes_controller.rb
+++ Juxtapose/app/controllers/votes_controller.rb
@@ -1,14 +1,22 @@
-class Votes < ApplicationController
+class VotesController < ApplicationController
 
 	 def gallery_vote
     vote = params[:vote]
     look_id = params[:look_id]
-    Vote.create(vote: vote look_id: look_id)
+    user_id = current_user.id
+    user_vote = Vote.find_by(look_id: look_id, user_id: user_id)
+    if user_vote
+    	user_vote.update(vote: vote)
+    else
+      Vote.create(vote: vote, look_id: look_id, user_id: user_id)
+    end
 
     respond_to do |format|
       format.json {render :nothing => true}
     end
   end
 
-  
+
+
+
 end
